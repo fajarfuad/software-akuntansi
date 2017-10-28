67050d79d9abb83d62e50fc1ed9dd01168f72a8d
fajarfuad <fajarfuad5@gmail.com>
2017-10-28 13:27:35 +0700

    user management

--- resources/views/user/user.blade.php
+++ resources/views/user/user.blade.php
@@ -11,10 +11,24 @@
   </ul>
   <div class="tab-content">
     <div class="tab-pane active" id="tab_1">
-      <h3>Tab 1</h3>
+      
     </div>
     <div class="tab-pane" id="tab_2">
-      //code tab_2
+      <form class="" action="index.html" method="post">
+        <div class="form-group">
+          <label>Password Lama</label>
+          <input type="password" class="form-control" name="old_pass" placeholder="Masukkan password lama">
+        </div>
+        <div class="form-group">
+          <label>Password Baru</label>
+          <input type="password" name="new_pass" class="form-control" placeholder="Masukkan Password baru">
+        </div>
+        <div class="form-group">
+          <label>Ulangi Password</label>
+          <input type="password" name="new_pass_again" class="form-control" placeholder="Ulangi Password baru">
+        </div>
+        <input type="submit" name="save" value="Save" class="btn btn-primary btn-flat">
+      </form>
     </div>
     <div class="tab-pane" id="tab_3">
       //code tab_3
