016f05642cd7e49c9b7aa99eec12dd0ce08e1d5d
fajarfuad <fajarfuad5@gmail.com>
2017-10-28 13:38:28 +0700

    tambah input

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -29,25 +29,22 @@
                 <div class="form-group">
                   <label class="col-sm-2 control-label">NPWP</label>
                   <div class="col-sm-6">
-                <input type="text" class="form-control" data-inputmask='"mask": "99 – 999 – 999 – 9 – 999 – 999"' data-mask>
+                <input type="text" placeholder="Masukkan NPWP" class="form-control" data-inputmask='"mask": "99 – 999 – 999 – 9 – 999 – 999"' data-mask>
               </div>
             </div>
             <div class="form-group">
               <label class="col-sm-2 control-label">Tanggal Pengukuhan NPWP</label>
               <div class="col-sm-6">
-            <input type="text" class="form-control" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask>
+            <input type="text" class="form-control" placeholder="Masukkan Tanggal" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask>
           </div>
         </div>
-
-
-
-              </div>
-              <!-- /.box-body -->
-              <div class="box-footer">
-                <button type="submit" class="btn btn-default">Cancel</button>
-                <button type="submit" class="btn btn-info">Sign in</button>
-              </div>
-              <!-- /.box-footer -->
-            </form>
-          </div>
+      </div>
+      <!-- /.box-body -->
+      <div class="box-footer">
+        <button type="submit" class="btn btn-default btn-flat">Cancel</button>
+        <button type="submit" class="btn btn-info btn-flat">Sign in</button>
+      </div>
+      <!-- /.box-footer -->
+    </form>
+  </div>
 @endsection
