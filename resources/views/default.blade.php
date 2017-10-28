e30edfbdabe1b6afca809c0edb1532d0a8ee1eae
fajarfuad <fajarfuad5@gmail.com>
2017-10-28 10:52:53 +0700

    change menu

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -227,11 +227,8 @@
         </li>
         <li class="treeview">
           <a href="#">
-            <i class="fa fa-files-o"></i>
-            <span>Layout Options</span>
-            <span class="pull-right-container">
-              <span class="label label-primary pull-right">4</span>
-            </span>
+            <i class="fa fa-gear"></i>
+            <span>Settings</span>
           </a>
           <ul class="treeview-menu">
             <li><a href="../layout/top-nav.html"><i class="fa fa-circle-o"></i> Top Navigation</a></li>
